df4143aff0c9b3a49dbbc9ff4f169f02255c17ae
Benjamin <therealbenpai@gmail.com>
2025-10-08 12:56:09 -0400

    fix index to say "About Me"

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@ const { data: sections } = await useFetch(`/api/about/get`);
 
 <template>
     <div class="flex flex-col mt-8 p-4 gap-4 overflow-y-auto">
-        <h1 class="text-4xl font-bold"></h1>
+        <h1 class="text-4xl font-bold">About Me</h1>
         <p class="text-lg">
             Hello! My name is Ben! I am a full stack developer who works on many
             applications. I am the owner of The FemDevs, a community of
